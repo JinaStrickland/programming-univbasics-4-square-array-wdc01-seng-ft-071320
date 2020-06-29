b793aed478ba2f395815fe5300cf644a3fc6966c
Jina Strickland <jina30@gmail.com>
2020-06-29 19:11:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -11,15 +11,6 @@ end
 
 
 =begin
-def square_array(array)
-  i = array.each
-  while array.each < array.length-1 do
-    new_array.collect(array.each ** array.each)
-  end
-  new_array
-end
-
-
 i = array.each
 array.collect { |i| i ** i }
 =end
