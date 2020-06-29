fd88e416aa9a2ab25e4ec38b995682e4a87b7a81
Jina Strickland <jina30@gmail.com>
2020-06-29 17:42:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,11 @@
 array = [1, 2, 3]
-
+new_array = [square_array(array).collect]
 def square_array(array)
   count = 0
   while array[count] do
     array[count] ** array[count]
     count += 1
+    
   end
+  new_array
 end
