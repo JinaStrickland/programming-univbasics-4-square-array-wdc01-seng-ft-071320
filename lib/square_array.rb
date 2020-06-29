8a4ae35eba4424bf23a09299e62193bed5f6f28c
Jina Strickland <jina30@gmail.com>
2020-06-29 16:12:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,6 +4,20 @@ new_array = []
 def square_array(array)
   while array.length do
     puts array.length.push(new_array)
+    new_array =
   end
   new_array
+end
+
+
+
+
+counter = 0
+array = [1, 2, 3]
+
+def square_array(array)
+  while array[counter] **  do
+
+    counter += 1
+  end
 end
