e42697d4ef163b0bbbb220d610c47aa29c06f647
Jina Strickland <jina30@gmail.com>
2020-06-29 18:15:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,9 +3,8 @@ new_array = []
 def square_array(array)
   count = 0
   while count < array.length-1 do
-    new_array.push(array[count] ** 2)
+    new_array.each(array[count] ** 2)
     count += 1
   end
   new_array
 end
-
