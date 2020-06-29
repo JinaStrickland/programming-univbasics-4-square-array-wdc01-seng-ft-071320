b68403c8b43367ab2b5ea93c5ec198956080299b
Jina Strickland <jina30@gmail.com>
2020-06-29 17:26:14 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -8,20 +8,29 @@ def square_array(array)
     new_array =
   end
   new_array
+
+
+    while array.each < array.length - 1 do
+      array.each[]
+      puts i  ** i
+
+    end
+    new_array[square_array(array)]
+  end
+
 =end
 
 
 
 array = [1, 2, 3]
-new_array = []
-def square_array(array)
-
-  while array.each do
-    i = array.each[]
-    puts i  ** i
-
-  end
-  new_array[square_array(array)]
+#def square_array(array)
+count = 0
+while array[count] do
+  puts array[count] + 1
+  count += 1
 end
+
+
+
 
 square_array(array)
